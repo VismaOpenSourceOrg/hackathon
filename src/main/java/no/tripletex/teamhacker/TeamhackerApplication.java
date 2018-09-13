ea2b4096a11ee272780b6568705173709090a4d0
Julian Ravn Thrap-Meyer <julian@tripletex.no>
2018-09-13 18:48:39 +0200

    Added actuator and disabled default repository config

--- src/main/java/no/tripletex/teamhacker/TeamhackerApplication.java
+++ src/main/java/no/tripletex/teamhacker/TeamhackerApplication.java
@@ -2,8 +2,11 @@ package no.tripletex.teamhacker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.rest.RepositoryRestMvcAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		RepositoryRestMvcAutoConfiguration.class
+})
 public class TeamhackerApplication {
 
 	public static void main(String[] args) {
