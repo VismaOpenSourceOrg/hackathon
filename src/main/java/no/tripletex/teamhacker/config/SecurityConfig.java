00aee4987ebea4dd21ef109e26868bcf2cd615f0
Julian Ravn Thrap-Meyer <julian@tripletex.no>
2018-09-15 15:31:07 +0200

    Added support for limiting OAuth domains

--- src/main/java/no/tripletex/teamhacker/config/SecurityConfig.java
+++ src/main/java/no/tripletex/teamhacker/config/SecurityConfig.java
@@ -1,19 +1,35 @@
 package no.tripletex.teamhacker.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+
+import java.util.List;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Bean
+	public CustomOidcUserService customOidcUserService() {
+		return new CustomOidcUserService();
+	}
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-				.anyRequest().authenticated()
+				.anyRequest().hasAuthority("ROLE_USER")
 				.and()
 				.csrf().disable()
 				.oauth2Login()
+				.userInfoEndpoint().oidcUserService(customOidcUserService())
+				.and()
 				.defaultSuccessUrl("/oauth-login-success", true);
 	}
 }
--- src/main/java/no/tripletex/teamhacker/config/WebConfig.java
+++ src/main/java/no/tripletex/teamhacker/config/WebConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
