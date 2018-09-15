c99a786375ca5a331fb37a4cdd6187eebc8f7adb
Julian Ravn Thrap-Meyer <julian@tripletex.no>
2018-09-15 19:19:13 +0200

    Moved header to its own component and converted to redux

---
+++ client/src/components/header.js
@@ -0,0 +1,36 @@
+// @flow
+
+import type { User } from "./user";
+import React from "react";
+
+import { connect } from "react-redux";
+import type { Idea } from "./idea";
+
+const HeaderComponent = (props: { auth: ?User }) => (
+  <div className="header">
+    <span className="header--title">Hackathon</span>
+    {props.auth && (
+      <div className="header--auth">
+        <span className="header--auth--email">{props.auth.email}</span>
+        <img className="header--auth--picture" src={props.auth.pictureUrl} />
+      </div>
+    )}
+  </div>
+);
+
+const mapStateToProps: any = state => {
+  return {
+    auth: state.auth
+  };
+};
+
+const mapDispatchToProps: any = dispatch => {
+  return {};
+};
+
+const ConnectedHeaderComponent = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(HeaderComponent);
+
+export default ConnectedHeaderComponent;
--- client/src/index.js
+++ client/src/index.js
@@ -9,8 +9,7 @@ import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 import createSagaMiddleware from "redux-saga";
 
-import type { User } from "./components/user";
-
+import ConnectedHeaderComponent from "./components/header";
 import ConnectedUserComponent from "./components/user";
 import ConnectedIdeaComponent from "./components/idea";
 
@@ -25,48 +24,13 @@ const store = createStore(
 );
 sagaMiddleware.run(saga);
 
-const Header = (props: { user: ?User }) => (
-  <div className="header">
-    <span className="header--title">Hackathon</span>
-    {props.user && (
-      <div className="header--auth">
-        <span className="header--auth--email">{props.user.email}</span>
-        <img className="header--auth--picture" src={props.user.pictureUrl} />
-      </div>
-    )}
+const Index = () => (
+  <div>
+    <ConnectedHeaderComponent />
+    <ConnectedIdeaComponent />
+    <ConnectedUserComponent />
   </div>
 );
-
-class Index extends React.Component<{}, { auth: ?User }> {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      auth: null
-    };
-  }
-
-  componentDidMount() {
-    fetch("/api/auth", { credentials: "same-origin" })
-      .then(response => response.json())
-      .then(data => {
-        this.setState({ ...this.state, auth: data });
-      });
-  }
-
-  render() {
-    const { auth } = this.state;
-    return (
-      <div>
-        <Header user={auth} />
-
-        <ConnectedIdeaComponent />
-
-        <ConnectedUserComponent />
-      </div>
-    );
-  }
-}
 
 const element = document.getElementById("main");
 if (element) {
