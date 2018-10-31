aeefedb5d309495d0f60836a632c00a65c21f5c8
Julian Ravn Thrap-Meyer <julian@tripletex.no>
2018-10-31 10:39:53 +0100

    Simple routing

--- client/src/index.js
+++ client/src/index.js
@@ -9,6 +9,14 @@ import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 import createSagaMiddleware from "redux-saga";
 
+import { createBrowserHistory } from "history";
+import {
+  connectRouter,
+  routerMiddleware,
+  ConnectedRouter
+} from "connected-react-router";
+import { Route, Switch } from "react-router";
+
 import ConnectedHeaderComponent from "./components/header";
 import ConnectedUserComponent from "./components/user";
 import ConnectedIdeaComponent from "./components/idea";
@@ -16,11 +24,14 @@ import ConnectedIdeaComponent from "./components/idea";
 import saga from "./store/saga";
 import reducer from "./store/reducer";
 
+const history = createBrowserHistory();
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(
-  reducer,
-  composeEnhancers(applyMiddleware(sagaMiddleware))
+  connectRouter(history)(reducer),
+  {},
+  composeEnhancers(applyMiddleware(routerMiddleware(history), sagaMiddleware))
 );
 sagaMiddleware.run(saga);
 
@@ -36,7 +47,13 @@ const element = document.getElementById("main");
 if (element) {
   ReactDOM.render(
     <Provider store={store}>
-      <Index />
+      <ConnectedRouter history={history}>
+        <Switch>
+          <Route exact path="/" render={() => <Index />} />
+          <Route exact path="/ideas" render={() => <Index />} />
+          <Route render={() => <div>No such page</div>} />
+        </Switch>
+      </ConnectedRouter>
     </Provider>,
     element
   );
