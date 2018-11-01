17977f3e2f48741916839230c3211b26bc0e0f86
Julian Ravn Thrap-Meyer <julian@tripletex.no>
2018-11-01 15:56:04 +0100

    Moved images to client folder

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: ["./src/index.js", "./sass/index.scss"],
@@ -24,7 +25,16 @@ module.exports = {
       template: "./templates/index.html",
       inject: "body",
       filename: "index.html"
-    })
+    }),
+    new CopyWebpackPlugin(
+      [
+        {
+          from: "images",
+          to: "images"
+        }
+      ],
+      {}
+    )
   ],
   module: {
     rules: [
