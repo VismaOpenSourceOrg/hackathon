97d71bc31ee059d032db0157a5f4a8cd256ea95d
Julian Ravn Thrap-Meyer <julian@tripletex.no>
2018-11-02 19:27:16 +0100

    Added deploy script

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -eo
+
+pushd client
+webpack --mode=production
+popd
+mvn package -DskipTests=true
+docker build -t 455710622635.dkr.ecr.eu-west-1.amazonaws.com/hackaton .
+docker push 455710622635.dkr.ecr.eu-west-1.amazonaws.com/hackaton
+
