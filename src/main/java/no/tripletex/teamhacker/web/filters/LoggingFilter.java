39534d64a23aab3f488026376f2865e1f6f2c8c2
Julian Ravn Thrap-Meyer <julian@tripletex.no>
2018-11-03 16:53:55 +0100

    Removed unused import

--- src/main/java/no/tripletex/teamhacker/web/filters/LoggingFilter.java
+++ src/main/java/no/tripletex/teamhacker/web/filters/LoggingFilter.java
@@ -5,7 +5,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
