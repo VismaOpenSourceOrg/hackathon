81ab851ff0068b18c363772de0e49aa97f75c405
Julian Ravn Thrap-Meyer <julian@tripletex.no>
2018-11-12 11:45:46 +0100

    Workaround to fix redis

--- src/main/java/com/visma/hackathon/config/WebConfig.java
+++ src/main/java/com/visma/hackathon/config/WebConfig.java
@@ -5,6 +5,7 @@ import com.visma.hackathon.web.interceptors.SuccessfulLoginInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.session.data.redis.config.ConfigureRedisAction;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -34,6 +35,13 @@ public class WebConfig implements WebMvcConfigurer {
 	@Bean
 	public LoggingFilter loggingFilter() {
 		return new LoggingFilter();
+	}
+
+	@Bean
+	public static ConfigureRedisAction configureRedisAction() {
+		// Disable the use of CONFIG against Redis, as this is not supported in AWS Managed Redis.
+		// Read more here: https://docs.spring.io/spring-session/docs/current/reference/html5/#api-redisoperationssessionrepository-sessiondestroyedevent
+		return ConfigureRedisAction.NO_OP;
 	}
 
 }
